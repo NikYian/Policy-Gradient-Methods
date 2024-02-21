a36f4bc7f96130f6cff262e3b9c781bbb654c406
NikYian <gnicksb@gmail.com>
2024-02-21 14:09:03 +0200

    a

--- plot_learning_curve.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt 
-import numpy as np
-
-
-def plot_learning_curve(scores, N=100, figure_file='plot'):
-    running_avg = np.convolve(scores, np.ones(N)/N, mode='valid') 
-    plt.plot(running_avg)
-    plt.title('Running Average of previous 100 scores')
-    plt.savefig(figure_file)
